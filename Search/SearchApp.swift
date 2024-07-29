41cc7b8bedf93b5279fc4874ee3df17303b696bf
Madi Muratuly <madimur2015@gmail.com>
2024-07-29 23:00:01 +0500

    Initial Commit

---
+++ Search/SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchApp.swift
+//  Search
+//
+//  Created by Madi Muratuly on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
